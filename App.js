2450d9d0f3af4faabc5bae4b4b716712c196d35c
Hamza SARI <hamza.sari@gmail.com>
2021-06-08 16:26:49 +0300

    @noriginmedia/react-spatial-navigation package has been added.

--- App.js
+++ App.js
@@ -8,7 +8,12 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { initNavigation, withFocusable } from '@noriginmedia/react-spatial-navigation';
+
+initNavigation({
+  nativeMode: true,
+});
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
@@ -18,6 +23,12 @@ const instructions = Platform.select({
   web: "Press Cmd+R to reload,\n" + "Cmd+Opt+i for dev menu"
 });
 
+const Box = ({ focused, setFocus }) => {
+  return <TouchableOpacity style={focused ? styles.focusedBox : styles.box}><Text>abc</Text></TouchableOpacity>;
+};
+
+const FocusableBox = withFocusable()(Box);
+
 export default class App extends Component {
   render() {
     return (
@@ -25,6 +36,9 @@ export default class App extends Component {
         <Text style={styles.welcome}>Welcome to React Native for Web!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <FocusableBox />
+        <FocusableBox />
+        <FocusableBox />
       </View>
     );
   }
@@ -46,5 +60,23 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "#333333",
     marginBottom: 5
+  },
+  box: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'red',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+  },
+  focusedBox: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'green',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
   }
 });
